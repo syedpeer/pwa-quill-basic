47d2ddaf8af7c168c48a0d8fe7ee4b2b2cdca2fd
Syed Imtiaz Peer <syed_peer@yahoo.com>
2020-03-11 05:27:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,88 @@
+class PWAConfApp {
+  constructor() {
+    this.speakersDiv = document.querySelector('.speakers');
+    this.scheduleDiv = document.querySelector('.schedule');
+    this.init();
+  }
+
+  async init() {
+    this.loadSpeakers();
+    this.loadSchedule();
+  }
+
+  async loadSpeakers() {
+    this.speakers = await this.fetchJSON('./data/speakers.json');
+
+    this.speakersDiv.innerHTML = this.speakers
+      .map(this.toSpeakerBlock)
+      .join('\n');
+  }
+
+  async loadSchedule() {
+    const rawSchedule = await this.fetchJSON('./data/schedule.json');
+
+    // Add speaker details to array
+    this.schedule = rawSchedule.map(this.addSpeakerDetails, this);
+    this.scheduleDiv.innerHTML = this.schedule
+      .map(this.toScheduleBlock)
+      .join('\n');
+  }
+
+  toSpeakerBlock(speaker) {
+    return `
+        <div class="speaker">
+          <img src="${speaker.picture}" alt="${speaker.name}">
+          <div>${speaker.name}</div>
+        </div>`;
+  }
+
+  toScheduleBlock(scheduleItem) {
+    return `
+      <div class="schedule-item ${scheduleItem.category}">
+        <div class="title-and-time">
+          <div class="time">${scheduleItem.startDate}</div>
+          <div class="title-and-speaker">
+            <div class="title">${scheduleItem.title}</div>
+            <div class="speaker">${
+              scheduleItem.speaker ? scheduleItem.speaker.name : '&nbsp;'
+            }</div>
+          </div>
+        </div>
+        <p class="description">${scheduleItem.description}</p>
+      </div>
+    `;
+  }
+
+  addSpeakerDetails(item) {
+    if (item.speakerId) {
+      return Object.assign({}, item, {
+        speaker: this.speakers.find(s => s.id === item.speakerId)
+      });
+    }
+    return Object.assign({}, item);
+  }
+
+  async fetchJSON(url) {
+    const res = await fetch(url);
+    return res.json();
+  }
+}
+window.addEventListener('load', e => {
+  new PWAConfApp();
+  registerSW()
+})
+
+async function registerSW() { 
+  //alert('Register the ServiceWorker..'); 
+  if ('serviceWorker' in navigator) { 
+    try {
+      await navigator.serviceWorker.register('./sw.js'); 
+    } catch (e) {
+      alert('ServiceWorker registration failed. Sorry about that.'); 
+    }
+  } else {
+    document.querySelector('.alert').removeAttribute('hidden'); 
+  }  
+}
+
+;
